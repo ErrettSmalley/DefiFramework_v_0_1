80f22f71e62efe6cab8915604ba9d802c37941fe
IlyaPrigogine <prigogine@protonmail.com>
2023-03-26 12:18:56 +0800

    Update 2_deploy_manager.ts

--- deploy/2_deploy_manager.ts
+++ deploy/2_deploy_manager.ts
@@ -3,44 +3,31 @@ import {DeployFunction} from "hardhat-deploy/types";
 const func: DeployFunction = async function ({deployments, getNamedAccounts, network, getChainId}) {
     const {deploy, execute, get} = deployments;
     const {owner} = await getNamedAccounts();
-
-    console.log(">> deploying routers...");
+    
     const Vault = await get("Vault");
     const Router = await get("Router");
     const WNative = await get("WNative");
-
     const ShortsTracker = await deploy("ShortsTracker", {
         from: owner,
         args: [Vault.address],
         log: true,
     });
-
-    console.log(`ShortTracer: ${ShortsTracker.address}`);
-
     const OrderBook = await deploy("OrderBook",{
         from: owner,
         args: [],
         log: true,
     });
-    console.log(`OrderBook: ${OrderBook.address}`);
-    
     const PositionManager = await deploy("PositionManager", {
         from: owner,
         args: [Vault.address, Router.address, ShortsTracker.address, WNative.address, 50, OrderBook.address],  // 5 / 1000
         log: true,
     });
-    console.log(`PositionManager: ${PositionManager.address}`);
-
-    console.log(">> shortsTracker setting...");
     await execute("ShortsTracker", {from: owner}, "setIsGlobalShortDataReady", true);
     await execute("ShortsTracker", {from: owner}, "setHandler", PositionManager.address, true);
-    
-    console.log(">> positionManager setting...");
     await execute("PositionManager", {from: owner}, "setOpened", true);
     await execute("Router", {from: owner}, "addPlugin", PositionManager.address);
     await execute("Timelock", {from: owner}, "setContractHandler", PositionManager.address, true);
     await execute("Timelock", {from: owner}, "setShouldToggleIsLeverageEnabled", true);
-
 };
 export default func;
 func.tags = ["manager"];
