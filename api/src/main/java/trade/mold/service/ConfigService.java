325687a8e7103f352653439ba21b95fb5c3ee99d
IlyaPrigogine <prigogine@protonmail.com>
2023-03-26 12:56:44 +0800

    submitted

--- api/src/main/java/trade/mold/service/ConfigService.java
+++
@@ -1,9 +0,0 @@
-package trade.mold.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface ConfigService {
-
-    Map<String, String> getConfigsByChainAndNames(long chain, List<String> names);
-}
--- api/src/main/java/trade/mold/service/ReaderService.java
+++
@@ -1,9 +0,0 @@
-package trade.mold.service;
-
-import java.math.BigDecimal;
-import java.util.Map;
-
-public interface ReaderService {
-
-    Map<String, BigDecimal> getCumulativeFundingRates(long chain) throws Exception;
-}
