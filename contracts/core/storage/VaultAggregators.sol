280ca297ec3cada8f6a4094cdfd2398e09d3288a
IlyaPrigogine <prigogine@protonmail.com>
2023-03-26 12:39:34 +0800

    submitted

--- contracts/core/storage/VaultAggregators.sol
+++ contracts/core/storage/VaultAggregators.sol
@@ -1,18 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "../../libraries/math/SafeMath.sol";
-import "../../libraries/token/IERC20.sol";
-import "../../libraries/token/SafeERC20.sol";
-import "../../libraries/DataTypes.sol";
-import "../../libraries/Constants.sol";
-import "../../libraries/Events.sol";
-import "../../libraries/Errors.sol";
-import "../../libraries/utils/ReentrancyGuard.sol";
-import "../interfaces/IVaultUtils.sol";
-import "../interfaces/IVault.sol";
-import "../interfaces/IVaultPriceFeed.sol";
-import "../../tokens/interfaces/IUSDM.sol";
-
-abstract contract VaultAggregators is IVault, ReentrancyGuard {
+abstract contract VaultAggregators {
 }
--- deploy/3_deploy_mlp.ts
+++ deploy/3_deploy_mlp.ts
@@ -6,44 +6,7 @@ const func: DeployFunction = async function ({deployments, getNamedAccounts, net
     const {owner} = await getNamedAccounts();
 
     // const Vault = await get("Vault");
-    // const USDM = await get("USDM");
-    // const WNative = await get("WNative");
-    // const MLP = await deploy("MLP", {
-    //     from: owner,
-    //     args: [],
-    //     log: true,
-    // });
-    // const MlpManager = await deploy("MlpManager", {
-    //     from: owner,
-    //     args: [Vault.address, USDM.address, MLP.address, AddressZero, 0], // TODO K
-    //     log: true,
-    // });
-    // const GMX = await deploy("GMX", {
-    //     from: owner,
-    //     args: [],
-    //     log: true,
-    // });
-    // const RewardRouter = await deploy("RewardRouter", {
-    //     contract: "RewardRouterV2",
-    //     from: owner,
-    //     args: [],
-    //     log: true,
-    // });
-    // const reader = await deploy("Reader", {
-    //     from: owner,
-    //     args: [],
-    //     log: true,
-    // });
-    // const vaultReader = await deploy("VaultReader", {
-    //     from: owner,
-    //     args: [],
-    //     log: true,
-    // });
-    //
-    // await execute("MLP", {from: owner}, "setMinter", MlpManager.address, true);
-    // await execute("RewardRouter", {from: owner}, "initialize", WNative.address, GMX.address, AddressZero, AddressZero, MLP.address, AddressZero, AddressZero, AddressZero, AddressZero, AddressZero, MlpManager.address, AddressZero, AddressZero);
-    // await execute("MlpManager", {from: owner}, "setHandler", RewardRouter.address, true);
-    // await execute("USDM", {from: owner}, "addVault", MlpManager.address);
+
 };
 export default func;
 func.tags = ["mlp"];
